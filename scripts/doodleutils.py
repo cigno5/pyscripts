cc41a8676a501081003bf6abb00712c70b4e8a47
Luca Cignoni <luca.cignoni@esa.int>
2020-01-30 08:44:18 +0100

    WIP

--- scripts/doodleutils.py
+++ scripts/doodleutils.py
@@ -23,7 +23,7 @@ def find_best_options():
 
         options = []
         people = []
-        preferences = [[] for _ in range(1, sheet.ncols)]
+        presences = [[] for _ in range(1, sheet.ncols)]
 
         for i, v in [(i, c.value) for i, c in enumerate(sheet.row(4))]:
             if i < 1:
@@ -38,11 +38,10 @@ def find_best_options():
                     people.append(person)
                     continue
 
-                preferences[c - 1].append(person if value == 'OK' else None)
+                presences[c - 1].append(person if value == 'OK' else None)
 
     print(options)
-    print(people)
-    print(preferences)
+    print(presences)
 
     # collect into groups
     def _c(_v):
@@ -54,8 +53,11 @@ def find_best_options():
             _ret = 1
         return _ret
 
+    sorted_presences = sorted(presences, key=lambda pres: len([p for p in pres if p]), reverse=True)
+
+    # grouping options by their presences
     count_group = {}
-    for idx, option in enumerate(preferences):
+    for idx, option in enumerate(presences):
         count = functools.reduce(lambda x, y: _c(x) + _c(y), option)
         if count in count_group:
             count_group[count].append(idx)
@@ -63,6 +65,27 @@ def find_best_options():
             count_group[count] = [idx]
 
     print(count_group)
+
+    for ref_opt_indexes in [v for _, v in sorted(count_group.items(), key=lambda it: it[0], reverse=True)]:
+        print("group.......")
+        for ref_opt_idx in ref_opt_indexes:
+            ref_presences = set([p for p in presences[ref_opt_idx] if p])
+            print("--------------------\nReference option: [%d] (count: %d) %s" % (ref_opt_idx, len(ref_presences), str(ref_presences)))
+
+            for cmp_pres_idx, _cmp_presences in [(i, p) for i, p in enumerate(presences) if i != ref_opt_idx]:
+                cmp_presences = set(p for p in _cmp_presences if p)
+                print("  Compare with #%03d (%3d items): %s" % (cmp_pres_idx, len(cmp_presences), str(cmp_presences)))
+
+                common_pref = cmp_presences.intersection(ref_presences)
+                print("     %3d items in common.......: %s" % (len(common_pref), str(common_pref)))
+
+                diff_pref = ref_presences.difference(cmp_presences)
+                print("     %3d items left (ref)......: %s" % (len(diff_pref), str(diff_pref)))
+
+                diff_pref = cmp_presences.difference(ref_presences)
+                print("     %3d items left (cmp)......: %s" % (len(diff_pref), str(diff_pref)))
+
+        break
 
 if __name__ == '__main__':
     _cmds = {
