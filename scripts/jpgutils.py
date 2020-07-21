02cf6b0f29850ffafb335878a65f2cb55cedf7f9
luca <luca.cignoni@gmail.com>
2020-07-21 23:02:28 +0200

    introduced catalog2 function with more intelligent approach

--- scripts/jpgutils.py
+++ scripts/jpgutils.py
@@ -111,6 +111,83 @@ def catalog():
     print("done")
 
 
+catalog_file = os.path.expanduser("~/.jpgutils-catalog.idx")
+
+
+def _load_catalog_index():
+    index = {}
+    if os.path.exists(catalog_file):
+        with open(catalog_file, 'r') as f:
+            first = True
+            for line in f.readlines():
+                if first:
+                    first = False
+                    continue
+                path, mtime = line[:-1].split('\t')
+                index[path] = float(mtime)
+
+    return index
+
+
+def _write_catalog_index(index: dict):
+    with open(catalog_file, 'w') as f:
+        f.write("original_path\ttimestamp%n")
+
+        for path, timestamp in index.items():
+            f.write("%s\t%s\n" % (path, timestamp))
+
+
+def catalog2():
+    def path_mtime():
+        return os.path.abspath(source_picture), os.path.getmtime(source_picture)
+
+    source = os.path.expanduser(args.source)
+    target = os.path.expanduser(args.target)
+    assert os.path.exists(source), "Source folder doesn't exist"
+
+    index = _load_catalog_index()
+
+    if not os.path.exists(target):
+        os.makedirs(target, exist_ok=True)
+
+    exts = ['.' + e for e in args.ext.split(",")]
+    forced_resize = args.force
+
+    for root, folders, files in os.walk(source):
+        print('scan %s...' % root)
+        if os.path.basename(os.path.normpath(root))[0] == '.':
+            continue
+
+        relative_root = root[len(source):]
+        for file in [f for f in files if os.path.splitext(f)[1].lower() in exts]:
+            source_picture = os.path.join(root, file)
+            target_picture = os.path.join(target, relative_root, file)
+            print('%s -> ' % source_picture, end='', flush=True)
+
+            path, mtime = path_mtime()
+            source_changed = path in index and index[path] != mtime
+            target_exists = os.path.exists(target_picture)
+
+            print('%s -> ' % target_picture, end='', flush=True)
+            if source_changed or forced_resize or not target_exists:
+                if target_exists:
+                    print('removing -> ', end='', flush=True)
+                    os.remove(target_picture)
+                print("coying -> ", end='', flush=True)
+                os.makedirs(os.path.dirname(target_picture), exist_ok=True)
+                shutil.copyfile(source_picture, target_picture)
+                print('resizing -> ', end='', flush=True)
+                subprocess.call(["mogrify", "-resize", args.resize, target_picture])
+                print('completed.')
+            else:
+                print("skipped.")
+
+            index[os.path.abspath(source_picture)] = os.path.getmtime(source_picture)
+
+    _write_catalog_index(index)
+    print("done")
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
@@ -134,6 +211,17 @@ if __name__ == '__main__':
     catalog_parser.add_argument("--force", action='store_true',
                                 help='Force resized file to be overridden')
 
+    catalog_parser2 = subparsers.add_parser("catalog2", help="Create catalog with smaller pictures")
+    catalog_parser2.set_defaults(command="catalog2")
+    catalog_parser2.add_argument("source", help="Original directory from where creating the catalog")
+    catalog_parser2.add_argument("target", help="Final destination of the catalog")
+    catalog_parser2.add_argument("--resize", default="1920x1080",
+                                 help="Resize value of the pictures (default 1920x1080")
+    catalog_parser2.add_argument("--ext", default="jpg,jpeg",
+                                 help="Extensions valid to be copied, separated by comma")
+    catalog_parser2.add_argument("--force", action='store_true',
+                                 help='Force resized file to be overridden')
+
     pshowcase_parser = subparsers.add_parser("permanent-showcase", help="Create permanent showcase")
     pshowcase_parser.set_defaults(command="permanent_showcase")
     pshowcase_parser.add_argument("target", help="Final destination of the catalog")
@@ -147,6 +235,8 @@ if __name__ == '__main__':
             showcase()
         elif args.command == 'catalog':
             catalog()
+        elif args.command == 'catalog2':
+            catalog2()
         elif args.command == 'permanent_showcase':
             permanent_showcase()
         else:
